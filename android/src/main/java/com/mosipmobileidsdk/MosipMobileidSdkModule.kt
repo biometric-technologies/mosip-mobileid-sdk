20d0a3363b9e9ac940bd4f6ef09412d91d74cd17
Slava Gornostal <slava.gornostal@gmail.com>
2023-02-28 07:40:29 +0200

    add biometric sdk android integration

--- android/src/main/java/com/mosipmobileidsdk/MosipMobileidSdkModule.kt
+++ android/src/main/java/com/mosipmobileidsdk/MosipMobileidSdkModule.kt
@@ -2,7 +2,6 @@ package com.mosipmobileidsdk
 
 import android.graphics.BitmapFactory
 import android.util.Base64
-import android.util.Log
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
@@ -14,9 +13,9 @@ import net.iriscan.sdk.face.FaceExtractProperties
 import net.iriscan.sdk.face.FaceMatchProperties
 import net.iriscan.sdk.face.FaceNetModelConfiguration
 import java.io.ByteArrayInputStream
-import java.io.InputStream
 
-
+// Example method
+// See https://reactnative.dev/docs/native-modules-android
 class MosipMobileidSdkModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
 
@@ -24,37 +23,47 @@ class MosipMobileidSdkModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
+  // https://github.com/biometric-technologies/biometric-sdk-android-example/blob/master/app/src/main/java/net/iriscan/sdktest/MainActivity.kt
+  @ReactMethod
+  fun init() {
+    BiometricSdkFactory.configure(
+      BiometricSdkConfigBuilder(this.reactApplicationContext)
+        .withFace(
+          FaceExtractProperties(),
+          FaceEncodeProperties(
+            faceNetModel = FaceNetModelConfiguration(
+              "https://github.com/biometric-technologies/biometric-sdk-android-example/blob/master/app/src/main/assets/facenet.tflite?raw=true",
+              160,
+              160,
+              128
+            )
+          ),
+          FaceMatchProperties(
+            threshold = 10.0
+          )
+        )
+        .build()
+    )
+  }
+
+
   @ReactMethod
   fun faceAuth(capturedImage: String, vcImage: String, promise: Promise) {
-    BiometricSdkFactory.configure(
-            BiometricSdkConfigBuilder(this.reactApplicationContext)
-                .withFace(
-                    FaceExtractProperties(),
-                    FaceEncodeProperties(),
-                    FaceMatchProperties()
-                )
-                .build()
-        )
     val sdk = BiometricSdkFactory.getInstance()
 
-    val decodedcapturedImage: ByteArray = Base64.decode(capturedImage, Base64.DEFAULT)
-    val capturedInputStream: InputStream = ByteArrayInputStream(decodedcapturedImage)
-    val capturedImageBmp = BitmapFactory.decodeStream(capturedInputStream)
-     val capturedImageTemplate = sdk.face()
-                .encoder()
-                .extractAndEncode(sdk.io().convert(capturedImageBmp))
+    val capturedBitmap =
+      BitmapFactory.decodeStream(ByteArrayInputStream(Base64.decode(capturedImage, Base64.DEFAULT)))
+    val vcBitmap =
+      BitmapFactory.decodeStream(ByteArrayInputStream(Base64.decode(vcImage, Base64.DEFAULT)))
 
-
-    val decodedvcImage: ByteArray = Base64.decode(vcImage, Base64.DEFAULT)
-    val vcInputStream: InputStream = ByteArrayInputStream(decodedvcImage)
-    val vcImageBmp = BitmapFactory.decodeStream(vcInputStream)
-    val vcImageTemplate = sdk.face()
+    val capturedTemplate = sdk.face()
       .encoder()
-      .extractAndEncode(sdk.io().convert(vcImageBmp))
+      .extractAndEncode(sdk.io().convert(capturedBitmap))
+    val vcTemplate = sdk.face()
+      .encoder()
+      .extractAndEncode(sdk.io().convert(vcBitmap))
 
-    promise.resolve(sdk.face().matcher().matches(capturedImageTemplate, vcImageTemplate))
+    promise.resolve(sdk.face().matcher().matches(capturedTemplate, vcTemplate))
   }
 
   companion object {
